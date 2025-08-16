a401a43a847080b0f93fcd9056e11a0a056b25f4
Nick Rothwell <nick@cassiel.com>
2025-08-16 20:21:29 +0100

    MIDI Fighter support.

--- midi-fighter.lua
+++ midi-fighter.lua
@@ -1,6 +1,7 @@
 -- ## midi-fighter
 -- Simple tests for MIDI Fighter Spectra,
 -- with colour support for the buttons.
+-- Select MIDI device via params page.
 --
 -- Nick Rothwell, nick@cassiel.com.
 
@@ -8,7 +9,9 @@
 G = { }
 
 BRIGHT = 15
-DIM = 1
+DIM = 4
+
+-- Obviously, will need to reload the script if the MIDI vports change.
 
 function init()
     local midi_devices = { }
@@ -16,10 +19,20 @@ function init()
     
     for i = 1, #midi.vports do
         midi_devices[i] = midi.connect(i)
+        -- The trim is mainly for the parameter page. (Perhaps we should
+        -- have a second table with longer names for the script page.))
         table.insert(
             midi_names,
-            "port "..i..": "..util.trim_string_to_width(midi_devices[i].name, 80)
+            "port "..i..": "..util.trim_string_to_width(midi_devices[i].name, 40)
         )
+        
+        -- Check Lua's closure capture: we should make sure we ignore
+        -- messages from devices other than the current target.
+        midi_devices[i].event =
+            function (x)
+                print("Virtual device " .. i)
+                tab.print(midi.to_msg(x))
+            end
     end
     
     params:add_option("midi target", "midi target", midi_names, 1)
@@ -30,6 +43,10 @@ function init()
         names = midi_names,
         target = 1
     }
+    
+    for i = 1, 16 do
+        G.midi.devices[G.midi.target]:note_on(36 + i - 1, math.random(127), 3)
+    end
 end
 
 local function print_item(row, key, value)
